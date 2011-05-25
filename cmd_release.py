eafb3fb4c43f15e415891dab711104876c0252b8
Alon Swartz <alon@sterilesecurity.com>
2011-05-25 11:45:10 +0300

    added: hubdns-release
    Release DNS records associated with FQDN (only TKLAPP.com)

---
+++ cmd_release.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# 
+# Copyright (c) 2011 Alon Swartz <alon@turnkeylinux.org>
+# 
+# This file is part of HubDNS
+# 
+# HubDNS is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+"""
+Release DNS records associated with FQDN (only TKLAPP.com)
+"""
+
+import sys
+import getopt
+
+from hubdns import HubDNS
+from utils import HubAPIError
+from registry import registry
+
+def fatal(e):
+    print >> sys.stderr, "error: " + str(e)
+    sys.exit(1)
+
+def usage(e=None):
+    if e:
+        print >> sys.stderr, "error: " + str(e)
+
+    print >> sys.stderr, "Syntax: %s" % sys.argv[0]
+    print >> sys.stderr, __doc__.strip()
+    sys.exit(1)
+
+def main():
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "h", ["help"])
+    except getopt.GetoptError, e:
+        usage(e)
+
+    for opt, val in opts:
+        if opt in ('-h', '--help'):
+            usage()
+
+    if args:
+        usage()
+
+    if not registry.sub_apikey and registry.fqdn:
+        fatal("not initialized")
+
+    try:
+        hubdns = HubDNS(subkey=registry.sub_apikey)
+        hubdns.release(registry.fqdn)
+    except HubAPIError, e:
+        fatal(e.description)
+
+    print "Released DNS records associated with %s" % registry.fqdn
+
+if __name__=="__main__":
+    main()
