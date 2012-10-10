afd97f54915885a0e2f0151864dae1bca4a8b94c
Liraz Siri <liraz@turnkeylinux.org>
2012-10-10 07:31:58 +0200

    increased sleep jitter from 5 minutes to 60 minutes

--- contrib/cron.sh
+++ contrib/cron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 # random sleep (max 5 min) to prevent clients from hitting the hub at the same time
-SLEEP=$[ ($RANDOM % 300) ] && sleep $SLEEP
+SLEEP=$[ ($RANDOM % 3600) ] && sleep $SLEEP
 
 hubdns-update --quiet
